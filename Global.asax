e629893f72886874965e99d2634c79fa778042ef
Franco Caldarola <franco.caldarola@hotmail.com>
2021-11-26 14:09:49 -0300

    Holis, probando probando

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Cochera.MvcApplication" Language="C#" %>
---
+++ Scripts/render.js
@@ -0,0 +1,9 @@
+﻿$(function () {
+
+    //Inicializo tooltips
+    $('[data-toggle="tooltip"]').tooltip();
+
+    //Inicializo popover
+    $('[data-toggle="popover"]').popover();
+
+});
